e854951cdf32fffbcec5d6248790d8a76586fcf4
Andres <alex@webthreeconsulting.com>
2024-03-19 17:38:54 -0500

    feat: intro card

---
+++ libs/fe-libs/src/lib/ui/components/button/button.tsx
@@ -0,0 +1,47 @@
+import styles from './button.module.scss';
+import { forwardRef } from 'react';
+import cn from 'classnames';
+
+import { MouseEventHandler, ReactNode } from 'react';
+
+export interface ButtonProps {
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  children: ReactNode;
+  type?: 'contained' | 'outlined' | 'round';
+  disabled?: boolean;
+  fullWidth?: boolean;
+  contrasted?: boolean;
+  className?: string;
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  function Button(props, ref) {
+    const {
+      onClick,
+      children,
+      type = 'contained',
+      disabled = false,
+      fullWidth = false,
+      contrasted = false,
+      className,
+    } = props;
+
+    return (
+      <button
+        disabled={disabled}
+        ref={ref}
+        className={cn(
+          styles['btn'],
+          { [styles['disabled']]: disabled },
+          { [styles['full-width']]: fullWidth },
+          { [styles['contrasted']]: contrasted },
+          styles[type],
+          className
+        )}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  }
+);
