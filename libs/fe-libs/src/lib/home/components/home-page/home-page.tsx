42e4782e2fb0059fc37279503df4290f581c01af
Miguel Montoya <stiquit@webthreeconsulting.com>
2024-03-19 18:53:08 -0500

    feat: create landing component

--- libs/fe-libs/src/lib/home/components/home-page/home-page.tsx
+++ libs/fe-libs/src/lib/home/components/home-page/home-page.tsx
@@ -1,16 +1,10 @@
 import styles from './home-page.module.scss';
-import { useTranslation } from 'react-i18next';
-import { useRouter } from '../../../routing/hooks/use-router';
+import { Landing } from '../landing/landing';
 
 export function HomePage() {
-  const { t, i18n } = useTranslation();
-  const { goToInventory } = useRouter();
   return (
     <div className={styles['container']}>
-      <h1>{t('home.welcome')}</h1>
-      <div onClick={goToInventory} style={{ cursor: 'pointer' }}>
-        {t('home.goToInventory')}
-      </div>
+      <Landing />
     </div>
   );
 }
