f1376f91d3c702bf32d0b3fa38ed74b21163b284
Miguel Montoya <stiquit@webthreeconsulting.com>
2024-03-19 13:56:51 -0500

    fix: set border for selected language

--- libs/fe-libs/src/lib/core/components/language-selector/language-selector.tsx
+++ libs/fe-libs/src/lib/core/components/language-selector/language-selector.tsx
@@ -2,6 +2,7 @@ import styles from './language-selector.module.scss';
 import LanguageIcon from '../../../../../assets/icons/language.svg';
 import { Button } from '../../../ui/components/button/button';
 import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
 
 export function LanguageSelector() {
   const { i18n } = useTranslation();
@@ -15,6 +16,9 @@ export function LanguageSelector() {
           key={index}
           onClick={() => i18n.changeLanguage(language)}
           type={'round'}
+          className={classNames(styles['button'], {
+            [styles['selected']]: i18n.language === language,
+          })}
         >
           {language.split('-')[0].toUpperCase()}
         </Button>
