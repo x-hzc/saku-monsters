d991dfe26f18263885032a296e7a3b33d0750c14
Miguel Montoya <stiquit@webthreeconsulting.com>
2024-03-21 18:05:33 -0500

    feat: create roadmap

--- libs/fe-libs/src/lib/home/components/home-page/home-page.tsx
+++ libs/fe-libs/src/lib/home/components/home-page/home-page.tsx
@@ -4,15 +4,26 @@ import { IntroCard } from '../../../cards/components/intro-card/intro-card';
 import { PhaseOne } from '../../../cards/components/phase-one/phase-one';
 import { GrowCard } from '../../../cards/components/grow-card/grow-card';
 import { PhaseTwo } from '../../../cards/components/phase-two/phase-two';
+import { Roadmap } from '../roadmap/roadmap';
 
 export function HomePage() {
   return (
     <div className={styles['container']}>
       <Landing />
       <IntroCard />
-      <PhaseOne />
-      <GrowCard />
+      <div className={styles['phase-one']}>
+        <PhaseOne />
+      </div>
+      <div className={styles['grow-card']}>
+        <div className={styles['card']}>
+          <GrowCard />
+        </div>
+        <div className={styles['bg-container']} />
+      </div>
       <PhaseTwo />
+      <div className={styles['roadmap']}>
+        <Roadmap />
+      </div>
     </div>
   );
 }
