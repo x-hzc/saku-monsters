53ea444c3021abb038082de018a6c2e32b85e9ca
Andres <alex@webthreeconsulting.com>
2024-03-22 16:11:18 -0500

    feat: footer

---
+++ libs/fe-libs/src/lib/footer/footer.tsx
@@ -0,0 +1,43 @@
+import styles from './footer.module.scss';
+import { useTranslation } from 'react-i18next';
+import { useRouter } from '../routing/hooks/use-router';
+import { Button } from '../ui/components/button/button';
+
+export function Footer() {
+  const { t } = useTranslation();
+  const { goToAppleSakuMonsters } = useRouter();
+
+  return (
+    <div className={styles['container']}>
+      <div className={styles['t-container']}>
+        <div className={styles['title']}>{t('footer.place')}</div>
+        <Button
+          className={styles['btn']}
+          onClick={goToAppleSakuMonsters}
+        >
+          TWITTER
+        </Button>
+      </div>
+      <ul className={styles['links-container']}>
+        <li className={styles['link']} onClick={goToAppleSakuMonsters}>
+          {t('footer.marketplace')}
+        </li>
+        <li className={styles['link']} onClick={goToAppleSakuMonsters}>
+          {t('footer.sakulog')}
+        </li>
+        <li className={styles['link']} onClick={goToAppleSakuMonsters}>
+          {t('footer.download')}
+        </li>
+        <li className={styles['link']} onClick={goToAppleSakuMonsters}>
+          {t('footer.privacy')}
+        </li>
+        <li className={styles['link']} onClick={goToAppleSakuMonsters}>
+          {t('footer.terms')}
+        </li>
+      </ul>
+      <div className={styles['rights']}>
+        © 2024 Saku Monsters, {t('footer.rights')}
+      </div>
+    </div>
+  );
+}
