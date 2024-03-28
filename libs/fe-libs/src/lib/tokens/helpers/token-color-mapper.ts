edccf4ea2f03905b33f6abab49561d0f56986f4e
Miguel Montoya <stiquit@webthreeconsulting.com>
2024-03-28 17:51:06 -0500

    fix: update monster images and center name from card

--- libs/fe-libs/src/lib/tokens/helpers/token-color-mapper.ts
+++ libs/fe-libs/src/lib/tokens/helpers/token-color-mapper.ts
@@ -5,15 +5,15 @@ import {
 } from '@saku-monsters/shared';
 
 export const TOKEN_RARITY_COLOR: Record<TokenRarityType, string> = {
-  [TokenRarityType.COMMON]: '#5CB561',
-  [TokenRarityType.RARE]: '#476AE0',
-  [TokenRarityType.EPIC]: '#9A5EE6',
-  [TokenRarityType.LEGENDARY]: '#C8974F',
+  [TokenRarityType.COMMON]: '#4ED29C',
+  [TokenRarityType.RARE]: '#5BAFFD',
+  [TokenRarityType.EPIC]: '#EB84F6',
+  [TokenRarityType.LEGENDARY]: '#FDC25B',
 };
 export const TOKEN_RARITY_BG_COLOR: Record<TokenRarityType, string> = {
   [TokenRarityType.COMMON]: '#A0F5D2',
   [TokenRarityType.RARE]: '#CCE3F9',
-  [TokenRarityType.EPIC]: '#EB84F6',
+  [TokenRarityType.EPIC]: '#F6DFFA',
   [TokenRarityType.LEGENDARY]: '#FEF1DB',
 };
 
